fbd1166053bab9e489806921685742e6a8249b7a
Stephen <stephen.c@bankbazaar.com>
2017-02-23 17:02:18 +0530

    changes

---
+++ getContainerHealth.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use LWP::Simple;
+use JSON;
+use Data::Dumper;
+
+my $service = $ARGV[0] or die "Please add service as argument\n";
+my $url = ' http://10.1.25.16:8500/v1/health/service/' . $service;
+
+my $response = (get $url);
+die "Error connecting to $url" unless defined $response;
+$response = decode_json ($response);
+
+my $filter;
+foreach my $key (@{$response}) {
+    foreach my $key2 (@{$key->{"Checks"}}) {
+        print Dumper ($key2);
+        $filter->{$key2->{'CheckID'}}->{$key2->{'Node'}} = $key2->{'Status'} 	
+    }
+}
+
+print Dumper ($filter);
