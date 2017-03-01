350cb5363d524f7bc5aa38995f7fec7e172b954d
Stephen <stephen.c@bankbazaar.com>
2017-03-01 17:25:59 +0530

    changes

---
+++ getBucketData.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+use strict;
+use LWP::Simple;
+use JSON;
+use Data::Dumper;
+use Sys::Hostname;
+
+my $tagno = $ARGV[0] or die "Please add <tagno> <bucket1|bucket2> <service> as arguments\n";
+my $bucket = $ARGV[1] or die "Please add <tagno> <bucket1|bucket2> <service> as arguments\n";
+my $service = $ARGV[2] or die "Please add <tagno> <bucket1|bucket2> <service> as arguments\n";
+
+my $path = $ENV{WORKSPACE} || "/tmp";
+my $bucketFile = "$path/$bucket-" . $tagno .".json";
+if (!(-e $bucketFile )) {
+    print "ERROR: Bucket File $bucketFile does not exist\n";
+    exit 1;
+}
+
+
+my $json;
+{
+  local $/; #Enable 'slurp' mode
+  open my $fh, "<", $bucketFile;
+  $json = <$fh>;
+  close $fh;
+}
+my $data = decode_json($json);
+
+if (defined $data->{$service}) {
+     print join(",", @{$data->{$service}})
+}
+else {
+    print "ERROR: No Data\n";
+    exit 1;
+}
