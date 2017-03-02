98e0c1cfa10edc653158310a9358e16b9000aa12
Stephen <stephen.c@bankbazaar.com>
2017-03-02 19:48:26 +0530

    changes

--- getContainerHealth.pl
+++ getContainerHealth.pl
@@ -4,7 +4,8 @@ use LWP::Simple;
 use JSON;
 use Data::Dumper;
 
-my $service = lc $ARGV[0] or die "Please add service as argument\n";
+my $service = lc $ARGV[0] or die "Please add service <list|all> as argument\n";
+my $list = lc $ARGV[1] or die "Please add service <list|all> as argument\n";
 my $url = ' http://10.1.25.16:8500/v1/health/service/' . $service;
 
 my $response = (get $url);
@@ -16,21 +17,26 @@ if (scalar(@$response) == 0) {
     exit 1;
 } 
 
+my @failed_hosts;
 my $filter;
 foreach my $key (@{$response}) {
     foreach my $key2 (@{$key->{"Checks"}}) {
        next if ( $key2->{'Status'} =~/passing/g);
-       $filter->{$key2->{'CheckID'}}->{$key2->{'Node'}} = $key2->{'Status'} 	
+       $filter->{$key2->{'CheckID'}}->{$key2->{'Node'}} = $key2->{'Status'} ;	
+       push @failed_hosts, $key2->{'Node'}
     }
 }
 
-if (defined ($filter)) {
+if ($list =~/list/) {
+    print join (",",@failed_hosts);
+    exit 1;
+}
+elsif (defined ($filter)) {
     print "Service " . $service . " : FAILURES " . Dumper ($filter) . "\n";
     exit 1;
-} else
+} 
+else
 {
     print "Service " . $service . " : Cluster Healthy" . "\n";
     exit 0;
 }
-
-
